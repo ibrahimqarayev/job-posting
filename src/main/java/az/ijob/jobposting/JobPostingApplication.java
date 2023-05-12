11f0938173cbb94c8d31dde79c5a1668910d1a06
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-05-13 00:34:47 +0400

    initial commit

---
+++ src/main/java/az/ijob/jobposting/JobPostingApplication.java
@@ -0,0 +1,13 @@
+package az.ijob.jobposting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobPostingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobPostingApplication.class, args);
+	}
+
+}
