761d76eeec723eeaf1ffde350141604c0c46de49
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-05-18 23:35:10 +0400

    Add LoginRequest

--- src/main/java/az/ijob/jobposting/dto/LoginRequest.java
+++ src/main/java/az/ijob/jobposting/dto/LoginRequest.java
@@ -1,5 +1,15 @@
 package az.ijob.jobposting.dto;
 
-public class LoginRequest {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class LoginRequest {
+    private String email;
+    private String password;
 }
