a1b1acd232cf2731e06a6a6ef40484cc3e58b8cc
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-05-18 23:44:57 +0400

    Add VacancyController

--- src/main/java/az/ijob/jobposting/contoller/VacancyController.java
+++ src/main/java/az/ijob/jobposting/contoller/VacancyController.java
@@ -1,4 +1,11 @@
 package az.ijob.jobposting.contoller;
 
+import az.ijob.jobposting.service.VacancyService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
 public class VacancyController {
+    private final VacancyService vacancyService;
 }
