e16c5687e9cc8d422d9309da62e01fdad1e1220b
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-05-18 23:35:21 +0400

    Add RegisterRequest

--- src/main/java/az/ijob/jobposting/dto/RegisterRequest.java
+++ src/main/java/az/ijob/jobposting/dto/RegisterRequest.java
@@ -1,4 +1,16 @@
 package az.ijob.jobposting.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class RegisterRequest {
+    private String username;
+    private String email;
+    private String password;
 }
