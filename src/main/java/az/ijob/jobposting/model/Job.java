4b6fe0acbaf0d9441fff37432d9bd9568e049161
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-05-19 23:56:01 +0400

    Rename

--- src/main/java/az/ijob/jobposting/model/Job.java
+++ src/main/java/az/ijob/jobposting/model/Job.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 @Table(name = "vacancies")
 @NoArgsConstructor
 @AllArgsConstructor
-public class Vacancy {
+public class Job {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
