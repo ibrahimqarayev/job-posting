c9bf3a1f7af44bb03b3b760169f719f5c974cfe7
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-05-19 23:56:20 +0400

    Rename

---
+++ src/main/java/az/ijob/jobposting/service/JobService.java
@@ -0,0 +1,13 @@
+package az.ijob.jobposting.service;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class JobService {
+
+
+
+}
