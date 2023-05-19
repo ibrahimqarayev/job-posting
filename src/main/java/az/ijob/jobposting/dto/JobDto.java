74e08795cc0d1de1dd9efd4f178a677be22da318
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-05-19 23:56:11 +0400

    Rename

--- src/main/java/az/ijob/jobposting/dto/JobDto.java
+++ src/main/java/az/ijob/jobposting/dto/JobDto.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class VacancyDto {
+public class JobDto {
     private Long id;
     private String name;
     private int salary;
