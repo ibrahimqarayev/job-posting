8c67939c804d4ed5ee0dab24e4746bf2cfd31f56
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-05-20 23:46:25 +0400

    Fix

--- src/main/java/az/ijob/jobposting/dto/JobDto.java
+++ src/main/java/az/ijob/jobposting/dto/JobDto.java
@@ -1,11 +1,13 @@
 package az.ijob.jobposting.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
