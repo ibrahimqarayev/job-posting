3b488e8b5b8263f92479060732116710acf40fec
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-05-25 23:22:08 +0400

    Add User

--- src/main/java/az/ijob/jobposting/model/User.java
+++ src/main/java/az/ijob/jobposting/model/User.java
@@ -1,4 +1,15 @@
 package az.ijob.jobposting.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String surname;
+    private String email;
+    private String password;
 }
