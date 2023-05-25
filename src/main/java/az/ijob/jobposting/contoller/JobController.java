43103fde8096e1d77f5303caa117c14390ffbd3f
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-05-25 23:55:22 +0400

    Fix

--- src/main/java/az/ijob/jobposting/contoller/JobController.java
+++ src/main/java/az/ijob/jobposting/contoller/JobController.java
@@ -10,9 +10,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class JobController {
     private final JobService jobService;
 
-    @GetMapping
-    public String home() {
+    @GetMapping("/index")
+    public String index() {
         return "index";
+    }
+
+    @GetMapping("/about")
+    public String about(){
+        return "about";
+    }
+
+    @GetMapping("/contact")
+    public String contact(){
+        return "contact";
+    }
+
+    @GetMapping("/companies")
+    public String companies(){
+        return "companies";
+    }
+
+    @GetMapping("/job-list")
+    public String jobListie(){
+        return "job-list";
     }
 
 }
