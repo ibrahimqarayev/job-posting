914f973c63c088008941ccc57f880949b348f58b
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-05-25 23:28:28 +0400

    Fix

--- src/main/java/az/ijob/jobposting/model/User.java
+++ src/main/java/az/ijob/jobposting/model/User.java
@@ -1,9 +1,15 @@
 package az.ijob.jobposting.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
