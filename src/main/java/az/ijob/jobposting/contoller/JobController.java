7e0be7f6fd8c4cce020b4695285d4165f0d9eb7f
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-05-29 23:41:55 +0400

    Add method

--- src/main/java/az/ijob/jobposting/contoller/JobController.java
+++ src/main/java/az/ijob/jobposting/contoller/JobController.java
@@ -1,9 +1,19 @@
 package az.ijob.jobposting.contoller;
 
+import az.ijob.jobposting.model.Job;
 import az.ijob.jobposting.service.JobService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -16,33 +26,65 @@ public class JobController {
     }
 
     @GetMapping("/companies")
-    public String companies(){
+    public String companies() {
         return "companies";
     }
 
     @GetMapping("/job-list")
-    public String jobListie(){
+    public String jobList(Model model) {
+        List<Job> jobs = jobService.findAll();
+        model.addAttribute("jobs", jobs);
         return "job-list";
     }
 
     @GetMapping("/about")
-    public String about(){
+    public String about() {
         return "about";
     }
 
     @GetMapping("/contact")
-    public String contact(){
+    public String contact() {
         return "contact";
     }
 
+    @GetMapping("/add")
+    public String addForm() {
+        return "add-job";
+    }
+
+    @PostMapping("/add-job")
+    public String addJob(
+            @RequestParam(name = "position",required = false) String position,
+            @RequestParam(name = "company",required = false) String company,
+            @RequestParam(name = "city",required = false) String city,
+            @RequestParam(name = "oHours",required = false) String oHours,
+            @RequestParam(name = "salary", required = false) Integer salary,
+            @RequestParam(name = "age", required = false) Integer age,
+            @RequestParam(name = "education", required = false) String education,
+            @RequestParam(name = "deadline", required = false) SimpleDateFormat deadline,
+            @RequestParam(name = "email", required = false) String email,
+            @RequestParam(name = "description", required = false) String description,
+            @RequestParam(name = "requirements", required = false) String requirements,
+            @RequestParam(name = "companyLogo", required = false) MultipartFile companyLogo
+    ) {
+        jobService.addJob(position, company, city,oHours, salary, age, education, deadline, email, description, requirements, companyLogo);
+        return "redirect:/job-list";
+    }
+
     @GetMapping("/register")
-    public String register(){
+    public String register() {
         return "register";
     }
 
     @GetMapping("/login")
-    public String login(){
+    public String login() {
         return "login";
     }
+
+    @GetMapping("xeta")
+    public String xeta() {
+        return "404";
+    }
+
 
 }
