da583f3d323af7227ad3c68b50f640200460321f
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-05-29 23:42:15 +0400

    Add fields

--- src/main/java/az/ijob/jobposting/dto/JobDto.java
+++ src/main/java/az/ijob/jobposting/dto/JobDto.java
@@ -5,18 +5,27 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
+import java.util.Date;
 
 @Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class JobDto {
+
     private Long id;
-    private String name;
-    private int salary;
+    private String position;
     private String company;
-    private String logo;
-    private String location;
-    private LocalDateTime creationTime;
+    public String city;
+    public String oHours;
+    private Integer salary;
+    private Integer age;
+    private String education;
+    private SimpleDateFormat deadline;
+    private String email;
+    private String description;
+    private String requirements;
+    private String companyLogo;
 }
