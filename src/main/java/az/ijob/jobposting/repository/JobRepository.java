ff96ceb4429b4c42485a9e4cdd4657247160814b
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-06-01 23:52:42 +0400

    Add method

--- src/main/java/az/ijob/jobposting/repository/JobRepository.java
+++ src/main/java/az/ijob/jobposting/repository/JobRepository.java
@@ -3,6 +3,19 @@ package az.ijob.jobposting.repository;
 import az.ijob.jobposting.model.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 
-public interface JobRepository extends JpaRepository<Job,Long> {
+
+public interface JobRepository extends JpaRepository<Job, Long> {
+    List<Job> findByPositionAndCategoryAndCity(String position, String category, String city);
+
+    List<Job> findByPositionAndCategory(String position, String category);
+
+    List<Job> findByPositionAndCity(String position, String city);
+
+    List<Job> findByCategoryAndCity(String category, String city);
+
+    List<Job> findByCategory(String category);
+
+    List<Job> findByCity(String city);
 }
