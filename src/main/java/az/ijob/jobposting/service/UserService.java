41432ac7d0a51776efa6c2aeedc70a5097aa18fb
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-06-09 23:55:11 +0400

    Add methods

--- src/main/java/az/ijob/jobposting/service/UserService.java
+++ src/main/java/az/ijob/jobposting/service/UserService.java
@@ -1,18 +1,21 @@
 package az.ijob.jobposting.service;
 
 import az.ijob.jobposting.dto.UserDto;
+import az.ijob.jobposting.exception.UserNotFoundException;
 import az.ijob.jobposting.model.User;
 import az.ijob.jobposting.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
 
-    public UserDto register(String name, String surname, String email, String password){
-        User user=new User();
+    public UserDto register(String name, String surname, String email, String password) {
+        User user = new User();
         user.setName(name);
         user.setSurname(surname);
         user.setEmail(email);
@@ -27,6 +30,11 @@ public class UserService {
                 .email(savedUser.getEmail())
                 .password(savedUser.getPassword())
                 .build();
+    }
+
+    public boolean login(String email, String password) {
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new UserNotFoundException("User not found !"));
+        return Objects.equals(user.getPassword(), password);
     }
 
 }
