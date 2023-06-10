c8e46eb01e4a38064fe29bee6e83657dad7eb261
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-06-10 23:58:21 +0400

    Add SecurityConfig

--- src/main/java/az/ijob/jobposting/config/SecurityConfig.java
+++ src/main/java/az/ijob/jobposting/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package az.ijob.jobposting.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -37,5 +38,9 @@ public class SecurityConfig {
         return http.build();
     }
 
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity httpSecurity) {
+
+    }
 
 }
