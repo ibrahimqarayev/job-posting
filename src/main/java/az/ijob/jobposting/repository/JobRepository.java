d5768ca106ff3d7d8ff46515d4119ea8d393dcea
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-06-13 23:43:33 +0400

    Clear

--- src/main/java/az/ijob/jobposting/repository/JobRepository.java
+++ src/main/java/az/ijob/jobposting/repository/JobRepository.java
@@ -7,15 +7,6 @@ import java.util.List;
 
 
 public interface JobRepository extends JpaRepository<Job, Long> {
-    List<Job> findByPositionAndCategoryAndCity(String position, String category, String city);
 
-    List<Job> findByPositionAndCategory(String position, String category);
 
-    List<Job> findByPositionAndCity(String position, String city);
-
-    List<Job> findByCategoryAndCity(String category, String city);
-
-    List<Job> findByCategory(String category);
-
-    List<Job> findByCity(String city);
 }
