0534e8486d238f00542d4f448f9e611482a7885c
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-06-14 23:56:59 +0400

    Add roles field

--- src/main/java/az/ijob/jobposting/model/User.java
+++ src/main/java/az/ijob/jobposting/model/User.java
@@ -5,6 +5,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "users")
 @Data
@@ -18,4 +21,12 @@ public class User {
     private String surname;
     private String email;
     private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "id")},
+            inverseJoinColumns = {@JoinColumn(name = "role_id", referencedColumnName = "id")}
+    )
+    private List<Role> roleList = new ArrayList<>();
 }
