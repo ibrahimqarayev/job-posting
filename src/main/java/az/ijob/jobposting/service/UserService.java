b7dba9c6bed56b9e7446e6b893677e83cadab067
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-06-15 23:03:13 +0400

    Add method

--- src/main/java/az/ijob/jobposting/service/UserService.java
+++ src/main/java/az/ijob/jobposting/service/UserService.java
@@ -1,40 +1,30 @@
 package az.ijob.jobposting.service;
 
-import az.ijob.jobposting.dto.UserDto;
-import az.ijob.jobposting.exception.UserNotFoundException;
+import az.ijob.jobposting.dto.RegistrationDto;
+import az.ijob.jobposting.model.Role;
 import az.ijob.jobposting.model.User;
+import az.ijob.jobposting.repository.RoleRepository;
 import az.ijob.jobposting.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.Objects;
+import java.util.Arrays;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
 
-    public UserDto register(String name, String surname, String email, String password) {
+    public void saveUser(RegistrationDto registrationDto) {
         User user = new User();
-        user.setName(name);
-        user.setSurname(surname);
-        user.setEmail(email);
-        user.setPassword(password);
-
-        User savedUser = userRepository.save(user);
-
-        return UserDto.builder()
-                .id(savedUser.getId())
-                .name(savedUser.getName())
-                .surname(savedUser.getSurname())
-                .email(savedUser.getEmail())
-                .password(savedUser.getPassword())
-                .build();
-    }
-
-    public boolean login(String email, String password) {
-        User user = userRepository.findByEmail(email).orElseThrow(() -> new UserNotFoundException("User not found !"));
-        return Objects.equals(user.getPassword(), password);
+        user.setName(registrationDto.getName());
+        user.setSurname(registrationDto.getSurname());
+        user.setEmail(registrationDto.getEmail());
+        user.setPassword(registrationDto.getPassword());
+        Role role = roleRepository.findByName("USER");
+        user.setRoleList(Arrays.asList(role));
+        userRepository.save(user);
     }
 
 }
