d951ed55d2970c566bc3cfe74ed50ce9c78ed31a
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-06-15 23:02:50 +0400

    Rename and move

--- src/main/java/az/ijob/jobposting/dto/LoginDto.java
+++ src/main/java/az/ijob/jobposting/dto/LoginDto.java
@@ -1,4 +1,4 @@
-package az.ijob.jobposting.request;
+package az.ijob.jobposting.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class LoginRequest {
+public class LoginDto {
     private String email;
     private String password;
 }
