33073a9f13480c036f7e0fa7e645769d1b2b1899
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-06-16 23:41:16 +0400

    Add method

--- src/main/java/az/ijob/jobposting/repository/UserRepository.java
+++ src/main/java/az/ijob/jobposting/repository/UserRepository.java
@@ -6,4 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
+
+    User findByUsername(String username);
+
+    User findFirstByUsername(String username);
 }
