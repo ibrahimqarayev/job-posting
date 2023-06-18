ac39b0f8eed375af818caef273ffdf55b308fe93
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-06-18 23:41:14 +0400

    Fix

--- src/main/java/az/ijob/jobposting/model/User.java
+++ src/main/java/az/ijob/jobposting/model/User.java
@@ -29,5 +29,5 @@ public class User {
             joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "id")},
             inverseJoinColumns = {@JoinColumn(name = "role_id", referencedColumnName = "id")}
     )
-    private List<Role> roleList = new ArrayList<>();
+    private List<Role> roles = new ArrayList<>();
 }
