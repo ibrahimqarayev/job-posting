a3d0bd0deda5d846ea362ad5ea9b4e0e19375ee6
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-07-05 21:45:38 +0400

    Delete

--- src/main/java/az/ijob/jobposting/repository/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package az.ijob.jobposting.repository;
-
-import az.ijob.jobposting.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-
-}
