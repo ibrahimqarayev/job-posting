66adef283ede57ea38545f2189304cb4c3a28ce5
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-07-06 11:36:03 +0400

    Add method

--- src/main/java/az/ijob/jobposting/service/UserService.java
+++ src/main/java/az/ijob/jobposting/service/UserService.java
@@ -1,12 +1,9 @@
 package az.ijob.jobposting.service;
 
-import az.ijob.jobposting.dto.LoginDto;
-import az.ijob.jobposting.dto.RegistrationDto;
-import az.ijob.jobposting.model.Role;
+import az.ijob.jobposting.dto.UserDto;
 import az.ijob.jobposting.model.User;
 import az.ijob.jobposting.repository.RoleRepository;
 import az.ijob.jobposting.repository.UserRepository;
-import ch.qos.logback.classic.servlet.LogbackServletContainerInitializer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -18,25 +15,21 @@ public class UserService {
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
 
-    public void saveUser(RegistrationDto registrationDto) {
+
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public User save(UserDto userDto) {
         User user = new User();
-        user.setName(registrationDto.getName());
-        user.setSurname(registrationDto.getSurname());
-        user.setEmail(registrationDto.getEmail());
-        user.setPassword(registrationDto.getPassword());
-        Role role = roleRepository.findByName("USER");
-        user.setRoles(Arrays.asList(role));
+        user.setName(userDto.getName());
+        user.setSurname(userDto.getSurname());
+        user.setEmail(userDto.getEmail());
+        user.setPassword(userDto.getPassword());
+        user.setRoles(Arrays.asList(roleRepository.findByName("USER")));
         userRepository.save(user);
+        return user;
     }
 
-    public boolean login(LoginDto loginDto) {
-        User user = userRepository.findByEmail(loginDto.getEmail());
-
-        if (user.getPassword().equals(loginDto.getPassword())) {
-            return true;
-        } else {
-            return false;
-        }
-    }
 
 }
