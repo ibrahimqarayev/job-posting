889506e2268fb3ed5ea159fe407d5b4e82697f0d
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-07-06 11:35:10 +0400

    Fix UserDto

--- src/main/java/az/ijob/jobposting/dto/UserDto.java
+++ src/main/java/az/ijob/jobposting/dto/UserDto.java
@@ -1,19 +1,24 @@
 package az.ijob.jobposting.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserDto {
-
-    private Long id;
+    @Size(min = 3,max = 15 ,message = "Ad uzunluğu 3-15 simvol aralığında olmalıdır")
     private String name;
+    @Size(min = 3,max = 20 ,message = "Soyad uzunluğu 3-20 simvol aralığında olmalıdır")
     private String surname;
+    @Email(message = "Email formatı düzgün deyil")
     private String email;
+    @Size(min = 8,max = 30 ,message = "Şifrə uzunluğu 8-30 simvol aralığında olmalıdır")
     private String password;
+    private String repeatPassword;
+
 }
