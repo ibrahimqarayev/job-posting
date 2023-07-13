dcc0df347a1d395596c2bfd86216921b526043fc
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-07-13 23:40:46 +0400

    Update JobConverter

--- src/main/java/az/ijob/jobposting/converter/JobConverter.java
+++ src/main/java/az/ijob/jobposting/converter/JobConverter.java
@@ -7,41 +7,52 @@ import org.springframework.stereotype.Component;
 @Component
 public class JobConverter {
 
-    public  JobDto entityToDto(Job job) {
-
-        return new JobDto(
-                job.getId(),
-                job.getCategory(),
-                job.getPosition(),
-                job.getCompany(),
-                job.getCity(),
-                job.getOHours(),
-                job.getSalary(),
-                job.getAge(),
-                job.getEducation(),
-                job.getEmail(),
-                job.getDescription(),
-                job.getRequirements(),
-                job.getCompanyLogo()
-        );
+    public JobDto entityToDto(Job job) {
+        JobDto jobDto = new JobDto();
+        jobDto.setId(job.getId());
+        jobDto.setCompany(job.getCompany());
+        jobDto.setCategory(job.getCategory());
+        jobDto.setPosition(job.getPosition());
+        jobDto.setSalary(job.getSalary());
+        jobDto.setDescription(job.getDescription());
+        jobDto.setRequirements(job.getRequirements());
+        jobDto.setEmploymentType(job.getEmploymentType());
+        jobDto.setCity(job.getCity());
+        jobDto.setAge(job.getAge());
+        jobDto.setEducation(job.getEducation());
+        jobDto.setRelevantPerson(job.getRelevantPerson());
+        jobDto.setPhoneNumber(job.getPhoneNumber());
+        jobDto.setEmail(job.getEmail());
+        jobDto.setCompanyLogo(job.getCompanyLogo());
+        jobDto.setCreationDate(job.getCreationDate());
+        jobDto.setExpirationDate(job.getExpirationDate());
+        jobDto.setNumberOfViews(job.getNumberOfViews());
+        jobDto.setSerialNumber(job.getSerialNumber());
+        return jobDto;
     }
 
-    public  Job dtoToEntity(JobDto jobDto) {
-        return new Job(
-                jobDto.getId(),
-                jobDto.getCategory(),
-                jobDto.getPosition(),
-                jobDto.getCompany(),
-                jobDto.getCity(),
-                jobDto.getOHours(),
-                jobDto.getSalary(),
-                jobDto.getAge(),
-                jobDto.getEducation(),
-                jobDto.getEmail(),
-                jobDto.getDescription(),
-                jobDto.getRequirements(),
-                jobDto.getCompanyLogo()
-        );
+    public Job dtoToEntity(JobDto jobDto) {
+        Job job = new Job();
+        job.setId(jobDto.getId());
+        job.setCompany(jobDto.getCompany());
+        job.setCategory(jobDto.getCategory());
+        job.setPosition(jobDto.getPosition());
+        job.setSalary(jobDto.getSalary());
+        job.setDescription(jobDto.getDescription());
+        job.setRequirements(jobDto.getRequirements());
+        job.setEmploymentType(jobDto.getEmploymentType());
+        job.setCity(jobDto.getCity());
+        job.setAge(jobDto.getAge());
+        job.setEducation(jobDto.getEducation());
+        job.setRelevantPerson(jobDto.getRelevantPerson());
+        job.setPhoneNumber(jobDto.getPhoneNumber());
+        job.setEmail(jobDto.getEmail());
+        job.setCompanyLogo(jobDto.getCompanyLogo());
+        job.setCreationDate(jobDto.getCreationDate());
+        job.setExpirationDate(jobDto.getExpirationDate());
+        job.setNumberOfViews(jobDto.getNumberOfViews());
+        job.setSerialNumber(jobDto.getSerialNumber());
+        return job;
     }
 
 }
