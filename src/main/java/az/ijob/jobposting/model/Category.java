47c789bc173d1f05e7b5354d1f267921b79726a8
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-07-21 23:11:11 +0400

    Update Category

--- src/main/java/az/ijob/jobposting/model/Category.java
+++ src/main/java/az/ijob/jobposting/model/Category.java
@@ -1,4 +1,18 @@
 package az.ijob.jobposting.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "categories")
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
 }
