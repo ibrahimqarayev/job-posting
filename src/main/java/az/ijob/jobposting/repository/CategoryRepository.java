eea64667458137b4c40929159570d541fe90ca5c
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-07-21 23:09:51 +0400

    Create CategoryRepository

---
+++ src/main/java/az/ijob/jobposting/repository/CategoryRepository.java
@@ -0,0 +1,10 @@
+package az.ijob.jobposting.repository;
+
+import az.ijob.jobposting.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findByName(String name);
+}
