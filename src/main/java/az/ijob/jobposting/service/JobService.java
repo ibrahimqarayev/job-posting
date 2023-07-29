768c380154e0a1e10113115c468d67cdc36e65b9
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-07-29 23:13:03 +0400

    Update

--- src/main/java/az/ijob/jobposting/service/JobService.java
+++ src/main/java/az/ijob/jobposting/service/JobService.java
@@ -2,7 +2,11 @@ package az.ijob.jobposting.service;
 
 
 import az.ijob.jobposting.dto.JobDto;
-import az.ijob.jobposting.exception.JobNotFoundException;
+import az.ijob.jobposting.dto.request.CreateJobRequest;
+import az.ijob.jobposting.dto.request.UpdateJobRequest;
+import az.ijob.jobposting.exception.ResourceNotFoundException;
+import az.ijob.jobposting.model.Category;
+import az.ijob.jobposting.model.Company;
 import az.ijob.jobposting.model.Job;
 import az.ijob.jobposting.repository.JobRepository;
 import lombok.RequiredArgsConstructor;
@@ -17,56 +21,56 @@ public class JobService {
     private final JobRepository jobRepository;
     private final ConverterService converter;
     private final CategoryService categoryService;
+    private final CompanyService companyService;
+
+    public JobDto addJob(CreateJobRequest createRequest) {
+        Job job = jobRepository.save(converter.toEntity(createRequest));
+        return converter.toDto(job);
+    }
 
     public List<JobDto> findAllJob() {
         return jobRepository.findAll()
-                .stream().map(converter::entityToDto).collect(Collectors.toList());
-    }
-
-    //
-    public JobDto addJob(JobDto jobDto) {
-        return converter.entityToDto(jobRepository.save(converter.dtoToEntity(jobDto)));
+                .stream().map(converter::toDto).collect(Collectors.toList());
     }
 
     public JobDto findById(Long jobId) {
-        return converter.entityToDto(getJobById(jobId));
+        return converter.toDto(jobRepository.findById(jobId).orElseThrow(() -> new ResourceNotFoundException("Job not found with id: " + jobId)));
     }
 
 
-    public JobDto updateJob(Long jobId, JobDto updateJob) {
+    public JobDto updateJob(Long jobId, UpdateJobRequest updateJob) {
         Job job = getJobById(jobId);
-        job.setCompany(updateJob.getCompany());
-        job.setCategory(categoryService.getCategoryById(updateJob.getCategoryId()));
+        job.setCompany(companyService.findByName(updateJob.getCompanyName()));
+        job.setCategory(categoryService.finById(updateJob.getCategoryId()));
         job.setPosition(updateJob.getPosition());
-        job.setSalary(updateJob.getSalary());
         job.setDescription(updateJob.getDescription());
         job.setRequirements(updateJob.getDescription());
         job.setEmploymentType(updateJob.employmentType);
         job.setCity(updateJob.getCity());
-        job.setAge(updateJob.getAge());
         job.setEducation(updateJob.getEducation());
         job.setRelevantPerson(updateJob.getRelevantPerson());
         job.setPhoneNumber(updateJob.getPhoneNumber());
         job.setEmail(updateJob.getEmail());
         job.setCompanyLogo(updateJob.getCompanyLogo());
-        return converter.entityToDto(jobRepository.save(job));
-}
+        return converter.toDto(jobRepository.save(job));
+    }
 
     public void deleteById(Long jobId) {
         jobRepository.deleteById(jobId);
     }
 
+
     public List<JobDto> searchJob(String query) {
-        return jobRepository.searchJob(query).stream().map(converter::entityToDto).collect(Collectors.toList());
+        return jobRepository.searchJob(query).stream().map(converter::toDto).collect(Collectors.toList());
     }
 
-    public List<JobDto> findByCategory(String category) {
-        List<Job> jobs = jobRepository.findByCategory(category);
-        return jobs.stream().map(converter::entityToDto).collect(Collectors.toList());
-    }
+//    public List<JobDto> findByCategory(String category) {
+//        List<Job> jobs = jobRepository.findByCategory(category);
+//        return jobs.stream().map(converter::toDto).collect(Collectors.toList());
+//    }
 
     protected Job getJobById(Long id) {
-        return jobRepository.findById(id).orElseThrow(() -> new JobNotFoundException("Job not found with id"));
+        return jobRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Job not found with id"));
     }
 
 }
