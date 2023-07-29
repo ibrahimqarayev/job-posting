bb414a9c4816d33fd4564f313ab48e39a314b918
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-07-29 22:35:22 +0400

    Update

--- src/main/java/az/ijob/jobposting/service/CompanyService.java
+++ src/main/java/az/ijob/jobposting/service/CompanyService.java
@@ -1,18 +1,20 @@
 package az.ijob.jobposting.service;
 
-import az.ijob.jobposting.exception.ResourceNotFoundException;
 import az.ijob.jobposting.model.Company;
 import az.ijob.jobposting.repository.CompanyRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class CompanyService {
     private final CompanyRepository companyRepository;
 
-    public Company findById(Long companyId) {
-        return companyRepository.findById(companyId).orElseThrow(() -> new ResourceNotFoundException("Company not found with id: " + companyId));
+    public Company findByName(String name) {
+        Optional<Company> optionalCompany = companyRepository.findByName(name);
+        return optionalCompany.orElseGet(() -> new Company(name));
     }
 
 }
