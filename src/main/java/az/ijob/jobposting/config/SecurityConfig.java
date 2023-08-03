05530ca1875ffa53512ee9c08cffd501b2068c63
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-08-03 23:50:00 +0400

    Update SecurityConfig

--- src/main/java/az/ijob/jobposting/config/SecurityConfig.java
+++ src/main/java/az/ijob/jobposting/config/SecurityConfig.java
@@ -2,34 +2,60 @@ package az.ijob.jobposting.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((authz) ->
-                        authz.requestMatchers("/admin")
-                                .authenticated()
-                                .anyRequest()
-                                .permitAll());
-        return http.build();
+    public UserDetailsService userDetailsService(){
+        return new UserDetailsServiceImpl();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .authorizeHttpRequests(auth ->
+                        auth
+                                .requestMatchers("/dashboard/**").authenticated()
+                                .anyRequest().permitAll())
+                .formLogin(formConfigurer -> {
+//                            formConfigurer.loginProcessingUrl("/login")
+//                                    .defaultSuccessUrl("/dashboard")
+//                                    .failureForwardUrl("/login")
+//                                    .loginPage("/login").defaultSuccessUrl("/dashboard");
+                            formConfigurer
+                                    .loginPage("/login")
+                                    .loginProcessingUrl("/login")
+                                    .defaultSuccessUrl("/my-jobs").permitAll();
+                        }
+
+
+                )
+//
+//
+//                .logout(logoutConfigurer -> {
+//                    logoutConfigurer
+//                            .invalidateHttpSession(true)
+//                            .clearAuthentication(true)
+//                            .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+//                            .logoutSuccessUrl("/login?logout").permitAll();
+//                })
+
+                .build();
     }
+
 
 }
