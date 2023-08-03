6cf1fa0b0ea47cce3897469e8c57d8c123769922
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-08-03 23:47:58 +0400

    Add UserDetailsServiceImpl

---
+++ src/main/java/az/ijob/jobposting/config/UserDetailsServiceImpl.java
@@ -0,0 +1,31 @@
+package az.ijob.jobposting.config;
+
+import az.ijob.jobposting.model.User;
+import az.ijob.jobposting.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("Could not find email");
+        }
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                user.getRoles()
+                        .stream()
+                        .map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList()));
+    }
+
+}
